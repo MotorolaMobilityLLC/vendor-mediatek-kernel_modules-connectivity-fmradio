c6b3b043f4beea3ef12f6423c952a3f902e1a128
Celeste Wang <celeste.wang@mediatek.com>
2019-02-27 18:36:02 +0800

    [ALPS03769744] build: use new build template to build fmradio_drv.ko
    [Detail]
    use build template under MTK_KERNEL_MODULE to build fmradio_drv.ko
    
    MTK-Commit-Id: b1530b31fa035d4379af7a6c6bdd88f01f34fcf2
    
    Change-Id: Ic23d78f5bfff13a02eefbfe605184e284b79d175
    CR-Id: ALPS03769744
    Feature: build process
    Signed-off-by: Celeste Wang <celeste.wang@mediatek.com>
    (cherry picked from commit dba0d6dfc546f375fa94a20a34c3b5b37e864f4b)

--- Android.mk
+++ Android.mk
@@ -1,106 +1,95 @@
+# Copyright Statement:
+#
+# This software/firmware and related documentation ("MediaTek Software") are
+# protected under relevant copyright laws. The information contained herein
+# is confidential and proprietary to MediaTek Inc. and/or its licensors.
+# Without the prior written permission of MediaTek inc. and/or its licensors,
+# any reproduction, modification, use or disclosure of MediaTek Software,
+# and information contained herein, in whole or in part, shall be strictly prohibited.
+#
+# MediaTek Inc. (C) 2017. All rights reserved.
+#
+# BY OPENING THIS FILE, RECEIVER HEREBY UNEQUIVOCALLY ACKNOWLEDGES AND AGREES
+# THAT THE SOFTWARE/FIRMWARE AND ITS DOCUMENTATIONS ("MEDIATEK SOFTWARE")
+# RECEIVED FROM MEDIATEK AND/OR ITS REPRESENTATIVES ARE PROVIDED TO RECEIVER ON
+# AN "AS-IS" BASIS ONLY. MEDIATEK EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NONINFRINGEMENT.
+# NEITHER DOES MEDIATEK PROVIDE ANY WARRANTY WHATSOEVER WITH RESPECT TO THE
+# SOFTWARE OF ANY THIRD PARTY WHICH MAY BE USED BY, INCORPORATED IN, OR
+# SUPPLIED WITH THE MEDIATEK SOFTWARE, AND RECEIVER AGREES TO LOOK ONLY TO SUCH
+# THIRD PARTY FOR ANY WARRANTY CLAIM RELATING THERETO. RECEIVER EXPRESSLY ACKNOWLEDGES
+# THAT IT IS RECEIVER'S SOLE RESPONSIBILITY TO OBTAIN FROM ANY THIRD PARTY ALL PROPER LICENSES
+# CONTAINED IN MEDIATEK SOFTWARE. MEDIATEK SHALL ALSO NOT BE RESPONSIBLE FOR ANY MEDIATEK
+# SOFTWARE RELEASES MADE TO RECEIVER'S SPECIFICATION OR TO CONFORM TO A PARTICULAR
+# STANDARD OR OPEN FORUM. RECEIVER'S SOLE AND EXCLUSIVE REMEDY AND MEDIATEK'S ENTIRE AND
+# CUMULATIVE LIABILITY WITH RESPECT TO THE MEDIATEK SOFTWARE RELEASED HEREUNDER WILL BE,
+# AT MEDIATEK'S OPTION, TO REVISE OR REPLACE THE MEDIATEK SOFTWARE AT ISSUE,
+# OR REFUND ANY SOFTWARE LICENSE FEES OR SERVICE CHARGE PAID BY RECEIVER TO
+# MEDIATEK FOR SUCH MEDIATEK SOFTWARE AT ISSUE.
+#
+# The following software/firmware and/or related documentation ("MediaTek Software")
+# have been modified by MediaTek Inc. All revisions are subject to any receiver's
+# applicable license agreements with MediaTek Inc.
+###############################################################################
+# Generally Android.mk can not get KConfig setting
+# we can use this way to get
+# include the final KConfig
+# but there is no $(AUTO_CONF) at the first time (no out folder) when make
+#
+#ifneq (,$(wildcard $(AUTO_CONF)))
+#include $(AUTO_CONF)
+#include $(CLEAR_VARS)
+#endif
+
+###############################################################################
+###############################################################################
+# Generally Android.mk can not get KConfig setting                            #
+#                                                                             #
+# do not have any KConfig checking in Android.mk                              #
+# do not have any KConfig checking in Android.mk                              #
+# do not have any KConfig checking in Android.mk                              #
+#                                                                             #
+# e.g. ifeq ($(CONFIG_MTK_COMBO_WIFI), m)                                     #
+#          xxxx                                                               #
+#      endif                                                                  #
+#                                                                             #
+# e.g. ifneq ($(filter "MT6632",$(CONFIG_MTK_COMBO_CHIP)),)                   #
+#          xxxx                                                               #
+#      endif                                                                  #
+#                                                                             #
+# All the KConfig checking should move to Makefile for each module            #
+# All the KConfig checking should move to Makefile for each module            #
+# All the KConfig checking should move to Makefile for each module            #
+#                                                                             #
+###############################################################################
+###############################################################################
+
 LOCAL_PATH := $(call my-dir)
 
+$(warning fmradio-MTK_FM_SUPPORT: [$(MTK_FM_SUPPORT)])
 ifeq ($(MTK_FM_SUPPORT),yes)
-
-ifeq ($(LINUX_KERNEL_VERSION),)
-    $(error LINUX_KERNEL_VERSION is not defined)
-endif
-
-KERNEL_ENV_PATH := $(LINUX_KERNEL_VERSION)
-ifndef KERNEL_ROOT_DIR
-KERNEL_ROOT_DIR := $(PWD)
-endif#KERNEL_ZIMAGE_OUT
-
 ifneq (true,$(strip $(TARGET_NO_KERNEL)))
-ifndef KERNEL_DIR
-  KERNEL_DIR := $(KERNEL_ENV_PATH)
-endif#KERNEL_DIR
-
-  ifeq ($(KERNEL_CROSS_COMPILE),)
-    ifeq ($(TARGET_ARCH), arm64)
-      KERNEL_CROSS_COMPILE := $(KERNEL_ROOT_DIR)/$(TARGET_TOOLS_PREFIX)
-    else
-      KERNEL_CROSS_COMPILE := $(KERNEL_ROOT_DIR)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-$(TARGET_GCC_VERSION)/bin/arm-eabi-
-    endif
-  endif
-
-    KERNEL_OUT ?= $(if $(filter /% ~%,$(TARGET_OUT_INTERMEDIATES)),,$(KERNEL_ROOT_DIR)/)$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
-  ifndef KERNEL_ZIMAGE_OUT
-    ifeq ($(TARGET_ARCH), arm64)
-      ifeq ($(MTK_APPENDED_DTB_SUPPORT), yes)
-        KERNEL_ZIMAGE_OUT := $(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/Image.gz-dtb
-      else
-        KERNEL_ZIMAGE_OUT := $(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/Image.gz
-      endif
-    else
-      ifeq ($(MTK_APPENDED_DTB_SUPPORT), yes)
-        KERNEL_ZIMAGE_OUT := $(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/zImage-dtb
-      else
-        KERNEL_ZIMAGE_OUT := $(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/zImage
-      endif
-    endif
-  endif#KERNEL_ZIMAGE_OUT
-
-  ifndef KERNEL_MAKE_OPTION
-    KERNEL_MAKE_OPTION := O=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) CROSS_COMPILE=$(KERNEL_CROSS_COMPILE) ROOTDIR=$(KERNEL_ROOT_DIR) $(if $(strip $(SHOW_COMMANDS)),V=1)
-  endif
-
-# check kernel folder exist
-ifeq (,$(wildcard $(KERNEL_DIR)))
-    $(error kernel $(KERNEL_DIR) is not existed)
-endif
-
-# check cross compiler exist
-ifeq (,$(wildcard $(KERNEL_CROSS_COMPILE)gcc))
-    $(error $(KERNEL_CROSS_COMPILE) is not existed)
-endif
-
-# set for module Makefile
-export AUTOCONF_H=$(KERNEL_OUT)/include/generated/autoconf.h
-
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := fmradio_drv.ko
-LOCAL_STRIP_MODULE := true
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_OWNER := mtk
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_TAGS := optional
-LOCAL_MULTILIB := first
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/modules
 LOCAL_INIT_RC := init.fmradio_drv.rc
 LOCAL_SRC_FILES := $(patsubst $(LOCAL_PATH)/%,%,$(shell find $(LOCAL_PATH) -type f -name '*.[cho]')) Makefile
 LOCAL_REQUIRED_MODULES := wmt_drv.ko
 
-include $(BUILD_SYSTEM)/base_rules.mk
+include $(MTK_KERNEL_MODULE)
 
 #### Copy Module.symvers from $(LOCAL_REQUIRED_MODULES) to this module #######
 #### For symbol link (when CONFIG_MODVERSIONS is defined)
-WMT_EXPORT_SYMBOL := $(subst $(LOCAL_MODULE),$(LOCAL_REQUIRED_MODULES),$(intermediates))/Module.symvers
-$(WMT_EXPORT_SYMBOL): $(subst $(LOCAL_MODULE),$(LOCAL_REQUIRED_MODULES),$(LOCAL_BUILT_MODULE))
-FMRADIO_EXPORT_SYMBOL := $(intermediates)/Module.symvers
-$(FMRADIO_EXPORT_SYMBOL).in: $(intermediates)/% : $(WMT_EXPORT_SYMBOL)
+WMT_EXPORT_SYMBOL := $(subst $(LOCAL_MODULE),$(LOCAL_REQUIRED_MODULES),$(intermediates)/LINKED)/Module.symvers
+$(WMT_EXPORT_SYMBOL): $(subst $(LOCAL_MODULE),$(LOCAL_REQUIRED_MODULES),$(linked_module))
+FMRADIO_EXPORT_SYMBOL := $(intermediates)/LINKED/Module.symvers
+$(FMRADIO_EXPORT_SYMBOL).in: $(intermediates)/LINKED/% : $(WMT_EXPORT_SYMBOL)
 	$(copy-file-to-target)
 	cp $(WMT_EXPORT_SYMBOL) $(FMRADIO_EXPORT_SYMBOL)
 
-LOCAL_GENERATED_SOURCES := $(addprefix $(intermediates)/,$(LOCAL_SRC_FILES))
-
-$(LOCAL_GENERATED_SOURCES): $(intermediates)/% : $(LOCAL_PATH)/% | $(ACP)
-	$(copy-file-to-target)
-
-$(KERNEL_OUT)/scripts/sign-file: $(KERNEL_ZIMAGE_OUT);
-
-$(LOCAL_BUILT_MODULE): KOUT := $(KERNEL_OUT)
-$(LOCAL_BUILT_MODULE): KOPTS := $(KERNEL_MAKE_OPTION) M=$(abspath $(intermediates))
-$(LOCAL_BUILT_MODULE): $(LOCAL_GENERATED_SOURCES) $(KERNEL_ZIMAGE_OUT)
-$(LOCAL_BUILT_MODULE): CERT_PATH := $(LINUX_KERNEL_VERSION)/certs
-$(LOCAL_BUILT_MODULE): $(wildcard $(LINUX_KERNEL_VERSION)/certs/ko_prvk.pem)
-$(LOCAL_BUILT_MODULE): $(wildcard $(LINUX_KERNEL_VERSION)/certs/ko_pubk.x509.der)
-$(LOCAL_BUILT_MODULE): $(wildcard vendor/mediatek/proprietary/scripts/kernel_tool/rm_ko_sig.py)
-$(LOCAL_BUILT_MODULE): $(KERNEL_OUT)/scripts/sign-file $(FMRADIO_EXPORT_SYMBOL).in
-	@echo $@: $^
-	$(MAKE) -C $(KOUT) $(KOPTS)
-	$(hide) $(call sign-kernel-module,$(KOUT)/scripts/sign-file,$(CERT_PATH)/ko_prvk.pem,$(CERT_PATH)/ko_pubk.x509.der)
-
+$(linked_module): $(FMRADIO_EXPORT_SYMBOL).in
 endif
 endif
