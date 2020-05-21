8b8096dbc4ef1ac8b96b6507728cdbc65ff17413
Holiday Hao <holiday.hao@mediatek.com>
2020-05-21 22:56:09 +0800

    [ALPS05117921] FM Radio: using kthread to read file
    Because android R cannot read file by user thread.
    So, it need to create kthread to read file.
    
    MTK-Commit-Id: 4dc843c9a3c69b87e75f25dee2662725329710d7
    
    Change-Id: Ie3e01522dddfcbeb630115d54699b89e4a94f470
    CR-Id: ALPS05117921
    Feature: [Module]FM Radio Driver
    Signed-off-by: Holiday Hao <holiday.hao@mediatek.com>

--- core/fm_patch.c
+++ core/fm_patch.c
@@ -13,11 +13,52 @@
 
 #include <linux/fs.h>
 #include <linux/firmware.h>
+#include <linux/jiffies.h>
+#include <linux/kthread.h>
 #include <linux/uaccess.h>
 
+#include "fm_patch.h"
 #include "fm_typedef.h"
 #include "fm_dbg.h"
 #include "fm_err.h"
+
+static struct fm_file_read_data g_file_read_data;
+
+int file_read_thread(void *arg)
+{
+	struct fm_file_read_data *data = (struct fm_file_read_data *)arg;
+	const struct firmware *fw = NULL;
+
+	if (!data)
+		return 0;
+
+	data->ret = request_firmware(&fw, data->filename, NULL);
+	if (data->ret) {
+		WCN_DBG(FM_ERR | CHIP, "Failed to load firmware \"%s\"\n",
+			data->filename);
+		release_firmware(fw);
+		data->ret = -FM_EPATCH;
+		return 0;
+	}
+	WCN_DBG(FM_NTC | CHIP, "load firmware \"%s\" ok\n", data->filename);
+
+	if (data->len >= fw->size) {
+		memcpy(data->dst, fw->data, fw->size);
+		WCN_DBG(FM_NTC | CHIP, "Copy file data(%p) size(%zu)\n",
+			fw->data, fw->size);
+		data->ret = fw->size;
+	} else {
+		WCN_DBG(FM_NTC | CHIP,
+			"Copy file data failed fw->size(%zu) > bufsize(%d)\n",
+			fw->size, data->len);
+		data->ret = -FM_EPATCH;
+	}
+	release_firmware(fw);
+
+	complete(&data->comp);
+
+	return 0;
+}
 
 /*
  * fm_file_read - read FM DSP patch/coeff/hwcoeff/rom binary file
@@ -29,27 +70,21 @@
  */
 signed int fm_file_read(const signed char *filename, unsigned char *dst, signed int len, signed int position)
 {
-	const struct firmware *fw = NULL;
-	signed int ret = 0;
+	struct fm_file_read_data *data = &g_file_read_data;
 
-	ret = request_firmware(&fw, filename, NULL);
-	if (ret) {
-		WCN_DBG(FM_ERR | CHIP, "Failed to load firmware \"%s\"\n", filename);
-		release_firmware(fw);
-		return -FM_EPATCH;
-	}
-	WCN_DBG(FM_NTC | CHIP, "load firmware \"%s\" ok\n", filename);
+	init_completion(&data->comp);
 
-	if (len >= fw->size) {
-		memcpy(dst, fw->data, fw->size);
-		WCN_DBG(FM_NTC | CHIP, "Copy file data(%p) size(%zu)\n", fw->data, fw->size);
-		ret = fw->size;
-	} else {
-		WCN_DBG(FM_NTC | CHIP, "Copy file data failed fw->size(%zu) > bufsize(%d)\n", fw->size, len);
-		ret = -FM_EPATCH;
-	}
-	release_firmware(fw);
-	return ret;
+	data->filename = filename;
+	data->dst = dst;
+	data->len = len;
+	data->position = position;
+	data->ret = 0;
+
+	kthread_run(file_read_thread, (void *)data, "file_read_thread", 0);
+
+	wait_for_completion_timeout(&data->comp, msecs_to_jiffies(3000));
+
+	return data->ret;
 }
 
 signed int fm_file_write(const signed char *filename, unsigned char *dst, signed int len, signed int *ppos)
