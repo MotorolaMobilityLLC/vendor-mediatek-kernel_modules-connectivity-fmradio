69ee3299509892fee49b29c2d127042619b7a6c1
Holiday Hao <holiday.hao@mediatek.com>
2021-01-28 14:33:51 +0800

    [ALPS05358429] FM Radio: add lna support
    add lna support for k80hd_ref_2g
    
    MTK-Commit-Id: 80e94d527e75a93fa6da2f24906bda8b94dff602
    
    Change-Id: Iea3c0ce669cc3485f8d2e3f50cfacb3a1f062fd5
    Signed-off-by: Kok Suan Lim <ks.lim@mediatek.com>
    CR-Id: ALPS05358429
    Feature: [Module]FM Radio Driver

--- core/fm_module.c
+++ core/fm_module.c
@@ -24,6 +24,8 @@
 #include <linux/sched.h>
 #include <linux/delay.h>	/* udelay() */
 #include <linux/version.h>
+#include <linux/gpio.h>
+#include <linux/of.h>
 
 #include "fm_config.h"
 #include "fm_main.h"
@@ -32,6 +34,9 @@
 #define FM_PROC_FILE		"fm"
 
 unsigned int g_dbg_level = 0xfffffff5;	/* Debug level of FM */
+
+#define FM_NO_LNA_PIN 0xffffffff
+unsigned int g_fm_lna_pin_num = FM_NO_LNA_PIN;
 
 /* fm main data structure */
 static struct fm *g_fm;
@@ -146,6 +151,14 @@ static long fm_ops_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 			else
 				fm_config.rx_cfg.deemphasis = 0;
 
+			if (g_fm_lna_pin_num == FM_NO_LNA_PIN) {
+				WCN_DBG(FM_NTC | MAIN, "%s: no fm lna\n", __func__);
+			} else {
+				gpio_set_value(g_fm_lna_pin_num, 1);
+				WCN_DBG(FM_NTC | MAIN, "%s: gpio_set_value %d: 1\n",
+					__func__, g_fm_lna_pin_num);
+			}
+
 			ret = fm_powerup(fm, &parm);
 			if (ret < 0) {
 				WCN_DBG(FM_NTC | MAIN, "FM_IOCTL_POWERUP:fail in fm_powerup, return\n");
@@ -174,6 +187,14 @@ static long fm_ops_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 			if (copy_from_user(&powerdwn_type, (void *)arg, sizeof(int))) {
 				ret = -EFAULT;
 				goto out;
+			}
+
+			if (g_fm_lna_pin_num == FM_NO_LNA_PIN) {
+				WCN_DBG(FM_NTC | MAIN, "%s: no fm lna\n", __func__);
+			} else {
+				gpio_set_value(g_fm_lna_pin_num, 0);
+				WCN_DBG(FM_NTC | MAIN, "%s: gpio_set_value %d: 0\n",
+					__func__, g_fm_lna_pin_num);
 			}
 
 			ret = fm_powerdown(fm, powerdwn_type);	/* 0: RX 1: TX */
@@ -1489,11 +1510,68 @@ static signed int fm_mod_destroy(struct fm *fm)
 	return ret;
 }
 
-static signed int mt_fm_probe(struct platform_device *pdev)
+static signed int fm_request_gpio(unsigned int pin)
 {
 	signed int ret = 0;
 
+	/* request gpio pin */
+	ret = gpio_request(pin, "fmlna");
+	if (ret == 0) {
+		WCN_DBG(FM_NTC | MAIN, "request gpio pin %d ok\n", pin);
+
+		/* set gpio direction to output */
+		gpio_direction_output(pin, 0);
+	} else {
+		if (ret == -EINVAL)
+			WCN_DBG(FM_ERR | MAIN, "gpio pin %d is not valid\n", pin);
+		else if (ret == -EBUSY)
+			WCN_DBG(FM_ERR | MAIN, "gpio pin %d is busy\n", pin);
+		else
+			WCN_DBG(FM_ERR | MAIN, "gpio pin %d unknown error\n", pin);
+	}
+
+	return ret;
+}
+
+static signed int mt_fm_probe(struct platform_device *pdev)
+{
+	signed int ret = 0;
+#ifdef CONFIG_OF
+	struct device_node *node = NULL, *pinctl_node = NULL, *pins_node = NULL;
+	unsigned int pin = 0;
+	signed int pin_ret = 0;
+#endif
+
 	WCN_DBG(FM_NTC | MAIN, "%s\n", __func__);
+
+#ifdef CONFIG_OF
+	node = of_find_compatible_node(NULL, NULL, "mediatek,fmradio");
+	if (!node)
+		WCN_DBG(FM_NTC | MAIN, "FM-OF: no fm device node\n");
+	else {
+		pinctl_node = of_parse_phandle(node, "pinctrl-1", 0);
+		if (pinctl_node) {
+			pins_node = of_get_child_by_name(pinctl_node, "pins_cmd_dat");
+			if (pins_node) {
+				pin_ret = of_property_read_u32(pins_node, "pins", &pin);
+				if (pin_ret)
+					WCN_DBG(FM_NTC | MAIN, "%s cannot find pins\n", __func__);
+				else {
+					g_fm_lna_pin_num = (pin >> 8) & 0xff;
+					WCN_DBG(FM_NTC | MAIN,
+						"%s FM LNA gpio pin number:%d, pinmux:0x%08x.\n",
+						__func__, g_fm_lna_pin_num, pin);
+
+					pin_ret = fm_request_gpio(g_fm_lna_pin_num);
+					if (pin_ret)
+						g_fm_lna_pin_num = FM_NO_LNA_PIN;
+				}
+			}
+		} else {
+			WCN_DBG(FM_ERR | MAIN, "%s get pinctrl-1 fail\n", __func__);
+		}
+	}
+#endif
 
 	ret = fm_mod_init(0);
 
@@ -1521,6 +1599,7 @@ static struct platform_device mt_fm_device = {
 	.id = -1,
 };
 */
+
 /* platform driver entry */
 static struct platform_driver mt_fm_dev_drv = {
 	.probe = mt_fm_probe,
@@ -1579,6 +1658,12 @@ static signed int mt_fm_init(void)
 static void mt_fm_exit(void)
 {
 	WCN_DBG(FM_NTC | MAIN, "%s\n", __func__);
+
+	if (g_fm_lna_pin_num != FM_NO_LNA_PIN) {
+		gpio_free(g_fm_lna_pin_num);
+		WCN_DBG(FM_NTC | MAIN, "free gpio pin %d ok\n", g_fm_lna_pin_num);
+	}
+
 	platform_driver_unregister(&mt_fm_dev_drv);
 	/* platform_device_unregister(&mt_fm_device); */
 	platform_device_unregister(pr_fm_device);
