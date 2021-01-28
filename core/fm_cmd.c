40be92c3b548dc004ccc3f58966d50b401a82c42
Mason Lai <mason.lai@mediatek.com>
2021-01-28 13:42:31 +0800

    [ALPS04097498] FM Radio: 6779 and 6635 driver porting
    [Detail]
    6779 and 6635 driver porting.
    
    MTK-Commit-Id: 04557681ae22a47adf7b6a2bceb8c3630fd9f69c
    
    Change-Id: If2de8fe8ba69f3899f36d12c36c0d45ac47c2770
    CR-Id: ALPS04097498
    Feature: FM Radio
    Signed-off-by: Mason Lai <mason.lai@mediatek.com>

--- core/fm_cmd.c
+++ core/fm_cmd.c
@@ -22,6 +22,12 @@
 #include "fm_link.h"
 #include "fm_cmd.h"
 
+#if defined(MT6635_FM)
+#define MCU_TOP_INDEX 05
+#else
+#define MCU_TOP_INDEX 04
+#endif
+
 signed int fm_bop_write(unsigned char addr, unsigned short value, unsigned char *buf, signed int size)
 {
 	if (size < (FM_WRITE_BASIC_OP_SIZE + 2)) {
@@ -142,7 +148,7 @@ signed int fm_bop_top_write(unsigned short addr, unsigned int value, unsigned ch
 
 	buf[0] = FM_TOP_WRITE_BASIC_OP;
 	buf[1] = FM_TOP_WRITE_BOP_SIZE;
-	buf[2] = 04;
+	buf[2] = MCU_TOP_INDEX;
 	buf[3] = (unsigned char) ((addr) & 0x00FF);
 	buf[4] = (unsigned char) ((addr >> 8) & 0x00FF);
 	buf[5] = (unsigned char) ((value) & 0x00FF);
@@ -172,7 +178,7 @@ signed int fm_bop_top_rd_until(unsigned short addr, unsigned int mask, unsigned 
 
 	buf[0] = FM_TOP_RD_UNTIL_BASIC_OP;
 	buf[1] = FM_TOP_RD_UNTIL_BOP_SIZE;
-	buf[2] = 04;
+	buf[2] = MCU_TOP_INDEX;
 	buf[3] = (unsigned char) ((addr) & 0x00FF);
 	buf[4] = (unsigned char) ((addr >> 8) & 0x00FF);
 	buf[5] = (unsigned char) ((mask) & 0x00FF);
@@ -424,7 +430,7 @@ signed int fm_top_get_reg(unsigned char *buf, signed int buf_size, unsigned shor
 	buf[1] = CSPI_READ_OPCODE;
 	buf[2] = 0x03;
 	buf[3] = 0x00;
-	buf[4] = 0x04;		/* top 04,fm 02 */
+	buf[4] = MCU_TOP_INDEX;
 	buf[5] = (unsigned char) ((addr) & 0x00FF);
 	buf[6] = (unsigned char) ((addr >> 8) & 0x00FF);
 
@@ -442,7 +448,7 @@ signed int fm_top_set_reg(unsigned char *buf, signed int buf_size, unsigned shor
 	buf[1] = CSPI_WRITE_OPCODE;
 	buf[2] = 0x07;
 	buf[3] = 0x00;
-	buf[4] = 0x04;		/* top 04,fm 02 */
+	buf[4] = MCU_TOP_INDEX;
 	buf[5] = (unsigned char) ((addr) & 0x00FF);
 	buf[6] = (unsigned char) ((addr >> 8) & 0x00FF);
 	buf[7] = (unsigned char) ((value) & 0x00FF);
